c34b04232810bcf82b8cb7ddbf9cdeb9d07800e7
Matt Giguere <matthew.giguere@gmail.com>
2015-10-06 10:19:29 -0700

    better handles unknown cities and protects against malicious input

--- code/flaskapp.py
+++ code/flaskapp.py
@@ -6,6 +6,29 @@ import sys
 import os
 import connect_aws_db as cadb
 
+
+available_cities = [
+    'new york city',
+    'los angeles',
+    'new haven',
+    'phoenix',
+    'chicago',
+    'houston',
+    'philadelphia',
+    'san antonio',
+    'san diego',
+    'dallas',
+    'san jose',
+    'austin',
+    'jacksonville',
+    'san francisco',
+    'palo alto',
+    'mountain view',
+    'sunnyvale',
+    'santa clara',
+    ]
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,10 +44,20 @@ def call_output():
     # gets the city variable from the linking page
     destination = request.args.get('ID')
 
-    hotel_names, hotel_ratings, hotel_imgs, hotel_urls, hotel_prices = rbh.retrieve_best_hotels2(destination)
-    return render_template("output.html",
-                           destination=destination, map_name='map4.html',
-                           ratings=zip(hotel_ratings, hotel_names, hotel_imgs, hotel_urls, hotel_prices))
+    # transform the destination to a more suitable format
+    # (i.e. remove state and make all lower case):
+    tdest = str(destination).split(',')[0].strip().lower()
+
+    if tdest in available_cities:
+        hotel_names, hotel_ratings, hotel_imgs, hotel_urls, hotel_prices = rbh.retrieve_best_hotels2(destination)
+        return render_template("output.html",
+                               destination=destination, map_name='map4.html', unavailable=False,
+                               ratings=zip(hotel_ratings, hotel_names, hotel_imgs, hotel_urls, hotel_prices))
+    else:
+        return render_template("output.html",
+                               destination=destination,
+                               unavailable=True,
+                               available_cities=available_cities)
 
 @app.route('/countme/<input_str>')
 def count_me(input_str):
--- flaskapp/app/views.py
+++ flaskapp/app/views.py
@@ -4,6 +4,27 @@ from flask import request
 import retrieve_best_hotels2 as rbh
 
 
+available_cities = [
+    'new york city',
+    'los angeles',
+    'new haven',
+    'phoenix',
+    'chicago',
+    'houston',
+    'philadelphia',
+    'san antonio',
+    'san diego',
+    'dallas',
+    'san jose',
+    'austin',
+    'jacksonville',
+    'san francisco',
+    'palo alto',
+    'mountain view',
+    'sunnyvale',
+    'santa clara',
+    ]
+
 @app.route('/')
 
 
@@ -17,10 +38,21 @@ def call_output():
     # gets the city variable from the linking page
     destination = request.args.get('ID')
 
-    hotel_names, hotel_ratings, hotel_imgs, hotel_urls, hotel_prices = rbh.retrieve_best_hotels2(destination)
-    return render_template("output.html",
-                           destination=destination, map_name='map4.html',
-                           ratings=zip(hotel_ratings, hotel_names, hotel_imgs, hotel_urls, hotel_prices))
+    # transform the destination to a more suitable format
+    # (i.e. remove state and make all lower case):
+    tdest = str(destination).split(',')[0].strip().lower()
+
+    if tdest in available_cities:
+        hotel_names, hotel_ratings, hotel_imgs, hotel_urls, hotel_prices = rbh.retrieve_best_hotels2(destination)
+        return render_template("output.html",
+                               destination=destination, map_name='map4.html', unavailable=False,
+                               ratings=zip(hotel_ratings, hotel_names, hotel_imgs, hotel_urls, hotel_prices))
+    else:
+        return render_template("output.html",
+                               destination=destination,
+                               unavailable=True,
+                               available_cities=available_cities)
+
     #       title = 'Home',
     #       user = user)
 
